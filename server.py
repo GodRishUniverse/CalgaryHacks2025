1c10020d57f423e67bdf84a9a0363421de44e956
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-02-15 12:53:03 -0700

    basic server edited
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import langchain as lc
 from langchain.chat_models import ChatOpenAI
 
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import TextLoader, PyPDFLoader, CSVLoader
 
 
 class Server:
@@ -9,5 +9,23 @@ class Server:
         self.llm = ChatOpenAI(temperature=1)    
         self.qa_chain = lc.load_qa_with_sources_chain(self.llm, chain_type="stuff")
 
-    def answer(self, question):
-        return self.qa_chain.run(input_documents=[], question=question)
+    def answer(self, question, documents):
+
+        doc_list = []
+        for doc in documents:
+            ext = doc.split(".")[-1]
+            if ext == "txt":
+                text_loader = TextLoader(documents)
+                text_docs = text_loader.load()
+                doc_list.append(text_docs)
+            elif ext == "pdf":
+                pdf_loader = PyPDFLoader(documents)
+                pdf_docs = pdf_loader.load()
+                doc_list.append(pdf_docs)
+            elif ext == "csv":
+                csv_loader = CSVLoader(documents)
+                csv_docs = csv_loader.load()
+                doc_list.append(csv_docs)
+
+           
+        return self.qa_chain.run(input_documents=doc_list, question=question)
